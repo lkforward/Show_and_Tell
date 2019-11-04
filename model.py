f1fce3a40e28e02207ced5d31124b879f623f9e2
Ke <lkforward@gmail.com>
2019-11-04 08:19:00 -0500

    A working version.
    After 3 epochs, the model performs reasonably well.

--- model.py
+++ model.py
@@ -23,14 +23,14 @@ class EncoderCNN(nn.Module):
         return features
 
 class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, batch_size, num_layers=1):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
         # input_gate = nn.Linear(embed_size, hidden_size)
         super(DecoderRNN, self).__init__()
 
         self.embed_size = embed_size
         self.hidden_size = hidden_size
         self.vocab_size = vocab_size
-        self.batch_size = batch_size
+        self.num_layers = num_layers
         
         self.embed1 = nn.Embedding(vocab_size, embed_size)
         self.lstm1 = nn.LSTM(input_size = embed_size, hidden_size = hidden_size, num_layers = num_layers)
@@ -47,31 +47,33 @@ class DecoderRNN(nn.Module):
             NOTE: We assume the captions all have the same length, the value in captions are integer indices. 
         """
         
-        embed_captions = self.embed1(captions)
+        # -> (n_batch, caption_length - 1, embed_size)
+        embed_captions = self.embed1(captions[:, :-1])
+
         # Expand the dim of features, so we have 3-dim as the inputs for LSTM:
+        # -> (n_batch, 1, embedding_size).
         features = features.unsqueeze(1)
         
         # Combine image feature with the caption sequence, since image feautre is served as the first one in the seq.
         # Also permute the dimension so dim_0 is number of elements in the sequence, and dim_1 is number of batches. 
-        # inputs = torch.cat([features, embed_captions], dim=1).permute(1, 0, 2)
-        inputs = torch.cat([features, embed_captions], dim=1)
+        # -> (caption_length, n_batch, embed_size)
+        inputs = torch.cat([features, embed_captions], dim=1).permute(1, 0, 2)
         print("The shape of lstm inputs:", inputs.shape)
         
-        hidden = (torch.zeros((1, self.batch_size, self.hidden_size), device=device), 
-                torch.zeros((1, self.batch_size, self.hidden_size), device=device))
-        lstm_out, _ = self.lstm1(inputs, hidden)
+        # hidden = (torch.zeros((1, self.batch_size, self.hidden_size), device=device), 
+        #         torch.zeros((1, self.batch_size, self.hidden_size), device=device))
+        # lstm_out, _ = self.lstm1(inputs, hidden)
+        lstm_out, _ = self.lstm1(inputs)
         print("The shape of lstm output:", lstm_out.shape)
-        # lstm_out has a shape (caption_length+1, n_batch, hidden_size)
-        # tag_space has a shape (caption_length+1, n_batch, vocabulary)
+
+        # (caption_length, n_batch, hidden_size) -> (caption_length, n_batch, vocabulary)
         tag_space = self.hidden2tag(lstm_out)
 
-        # The prediction of the sentence starts from the 2nd output:
-        tag_space = tag_space[1:, :, :]
+        # We have dropped the last element in the caption at the beginning, so no need to drop again here:
+        # tag_space = tag_space[1:, :, :]
         # # Put batch_size back to dim-0, and the new tag_space has a shape (n_batch, caption_length, vocabulary): 
-        # tag_space = tag_space.permute(1, 0, 2)
-        
-        # # Apply the f(x) = log(softmax(x)) to the dim for a word: 
-        # tag_scores = F.log_softmax(tag_space, dim=-1)
+        tag_space = tag_space.permute(1, 0, 2)
+        print("The shape of tag_space:", tag_space.shape)
 
         return tag_space
 
@@ -91,14 +93,13 @@ class DecoderRNN(nn.Module):
         prd_words = []
         while len(prd_words) < (max_len - 1):
             o_t, hidden = self.lstm1(inputs, hidden)
-            tag_space = self.hidden2tag(o_t)
-            tag_scores = F.log_softmax(tag_space, dim=-1)
-            tag_scores = tag_scores.squeeze(1)
+            tag_space = self.hidden2tag(o_t)  #  1,1,vocab_size
+            tag_scores = tag_space.squeeze(1)  # 1,vocab_size
 
-            word_ind = tag_scores.argmax(dim=-1) 
+            word_ind = tag_scores.argmax(dim=-1)  # 1
             prd_words.append(word_ind.item()) 
 
-            inputs = self.embed1(word_ind.unsqueeze(0))
+            inputs = self.embed1(word_ind.unsqueeze(0)) # 1,1->1,1,embed_size
 
         # End of the prediction: 
         prd_words.append(end_token)
