0ddcd81c07a46071dd43c0afe43b8124b8215774
Ke <lkforward@gmail.com>
2019-11-03 20:41:06 -0500

    Draft version, some ideas from Sebastian

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
@@ -22,13 +23,14 @@ class EncoderCNN(nn.Module):
         return features
 
 class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+    def __init__(self, embed_size, hidden_size, vocab_size, batch_size, num_layers=1):
         # input_gate = nn.Linear(embed_size, hidden_size)
         super(DecoderRNN, self).__init__()
 
         self.embed_size = embed_size
         self.hidden_size = hidden_size
         self.vocab_size = vocab_size
+        self.batch_size = batch_size
         
         self.embed1 = nn.Embedding(vocab_size, embed_size)
         self.lstm1 = nn.LSTM(input_size = embed_size, hidden_size = hidden_size, num_layers = num_layers)
@@ -47,14 +49,17 @@ class DecoderRNN(nn.Module):
         
         embed_captions = self.embed1(captions)
         # Expand the dim of features, so we have 3-dim as the inputs for LSTM:
-        features = features.view(features.shape[0], 1, features.shape[1])
-
+        features = features.unsqueeze(1)
+        
         # Combine image feature with the caption sequence, since image feautre is served as the first one in the seq.
         # Also permute the dimension so dim_0 is number of elements in the sequence, and dim_1 is number of batches. 
-        inputs = torch.cat([features, embed_captions], dim=1).permute(1, 0, 2)
+        # inputs = torch.cat([features, embed_captions], dim=1).permute(1, 0, 2)
+        inputs = torch.cat([features, embed_captions], dim=1)
         print("The shape of lstm inputs:", inputs.shape)
         
-        lstm_out, _ = self.lstm1(inputs)
+        hidden = (torch.zeros((1, self.batch_size, self.hidden_size), device=device), 
+                torch.zeros((1, self.batch_size, self.hidden_size), device=device))
+        lstm_out, _ = self.lstm1(inputs, hidden)
         print("The shape of lstm output:", lstm_out.shape)
         # lstm_out has a shape (caption_length+1, n_batch, hidden_size)
         # tag_space has a shape (caption_length+1, n_batch, vocabulary)
@@ -62,13 +67,13 @@ class DecoderRNN(nn.Module):
 
         # The prediction of the sentence starts from the 2nd output:
         tag_space = tag_space[1:, :, :]
-        # Put batch_size back to dim-0, and the new tag_space has a shape (n_batch, caption_length, vocabulary): 
-        tag_space = tag_space.permute(1, 0, 2)
+        # # Put batch_size back to dim-0, and the new tag_space has a shape (n_batch, caption_length, vocabulary): 
+        # tag_space = tag_space.permute(1, 0, 2)
         
-        # Apply the f(x) = log(softmax(x)) to the dim for a word: 
-        tag_scores = F.log_softmax(tag_space, dim=-1)
+        # # Apply the f(x) = log(softmax(x)) to the dim for a word: 
+        # tag_scores = F.log_softmax(tag_space, dim=-1)
 
-        return tag_scores
+        return tag_space
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
